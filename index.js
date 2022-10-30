b3868734697473286ab5353243ae17b386fcf9cd
Abdullah <54276938+Abdul535@users.noreply.github.com>
2022-10-30 13:57:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express') 
+require('dotenv').config()
+console.log(process.env)
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send(process.env.MONGO_URI)
 })
 app.listen(process.env.PORT || 3000)
